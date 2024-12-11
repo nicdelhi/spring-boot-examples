edffe489d403be135d0c3387ec809c28be93a2a8
nicdelhi <45124237+nicdelhi@users.noreply.github.com>
2024-12-11 21:01:54 +1100

    Update WeatherApplication.java

--- weather-service/src/main/java/com/example/springboot/microservice/WeatherApplication.java
+++ weather-service/src/main/java/com/example/springboot/microservice/WeatherApplication.java
@@ -1,3 +1,4 @@
+package com.example.springboot.weather-service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
